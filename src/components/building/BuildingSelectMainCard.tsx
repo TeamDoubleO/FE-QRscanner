f0d823d0af9679f20d358dea984794c19d5e9ffb
문재연 <jaeyeonmando@gmail.com>
2025-05-28 14:22:06 +0900

    KW-516/feat: 선택 메인 카드 추가

---
+++ src/components/building/BuildingSelectMainCard.tsx
@@ -0,0 +1,12 @@
+import './css/BuildingSelectMainCard.css';
+
+const BuildingSelectMainCard: React.FC = () => {
+  return (
+    <div className="building-select-main-card">
+      <h2>건물 선택</h2>
+      <p>내용 배치</p>
+    </div>
+  );
+};
+
+export default BuildingSelectMainCard;
