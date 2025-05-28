54be30bf139852cec93bf25371eb759266c84273
문재연 <jaeyeonmando@gmail.com>
2025-05-28 14:49:56 +0900

    KW-487/feat: 선택 페이지 애니메이션 구현 및 연결

--- src/components/zone/ZoneSelectWhiteCard.tsx
+++ src/components/zone/ZoneSelectWhiteCard.tsx
@@ -3,8 +3,6 @@ import './css/ZoneSelectWhiteCard.css';
 const ZoneSelectWhiteCard: React.FC = () => {
   return (
     <div className="zone-select-white-card">
-      <h2>Zone 선택</h2>
-      <p>내용 배치</p>
     </div>
   );
 };
